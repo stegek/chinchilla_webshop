993021fc56837f225bf68082e465294b8a027b8f
Karl Steger <stegek11@gmx.at>
2024-07-12 20:38:10 +0200

    style navbar include searchbar in nav whithout logic

--- frontend/client/src/components/Navbar.js
+++ frontend/client/src/components/Navbar.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [navSearch, setNavSearch] = useState("");
+
+  const handleNavSearch = (e) => {
+    setNavSearch(e.target.value);
+  };
+
   return (
     <nav>
       <ul>
         <li>
           <Link to="/">
-            <img className="route-img" src="home.png" alt="home" />
+            <img className="home-img" src="homechin.png" alt="home" />
           </Link>
         </li>
         <li className="nav-item">dummy1</li>
         <li className="nav-item">dummy2</li>
         <li className="nav-item">dummy3</li>
-        <li className="nav-item">dummy4</li>
+        <li className="nav-search">
+          <input
+            type="text"
+            placeholder="Enter productname ..."
+            value={navSearch}
+            onChange={handleNavSearch}
+            className="search-input"
+          ></input>
+        </li>
         <li className="nav-item">dummy5</li>
         <li className="nav-item">dummy6</li>
 
         <li className="navbar-cart">
           <Link to="/warenkorb">
-            <img className="route-img" src="cart.png" alt="cart" />
+            <img className="route-img" src="whitecart.png" alt="cart" />
           </Link>
         </li>
       </ul>
